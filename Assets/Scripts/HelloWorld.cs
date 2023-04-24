e18c4d09e3319281f4b029cd84f5c86f77825053
TranafLee <tranthao2492002@gmail.com>
2023-04-24 14:15:01 +0700

    nothing

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 public class HelloWorld : MonoBehaviour
 {
     private void Awake() {
-        Debug.Log("Hello my name is Duc.");
+        Debug.Log("my name is Duc.");
     }
 }
