45580b18d4f0e94b308d55b65ef63c4abf5393fa
QuangDuc-HUST <ducquangng.03@gmail.com>
2023-06-16 19:37:03 +0700

    Reponsive Camera features

---
+++ Assets/Scripts/GameManager/CameraScaling.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScaling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float developedHorizontalResolution = 1242;
+    [SerializeField] private float developedVerticalResolution = 2688;
+
+    private float initSize;
+
+    private void Awake() {
+        initSize = Camera.main.orthographicSize;
+    }
+
+    private void OnGUI() {
+
+
+        float currentAspect = (float) Screen.width / (float) Screen.height;
+		Camera.main.orthographicSize = developedHorizontalResolution / currentAspect / (developedVerticalResolution / initSize);
+	}
+}
+
