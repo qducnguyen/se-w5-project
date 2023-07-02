274fecc0c2d83e8f4e4a64472ab10da1112e3267
TranafLee <tranthao2492002@gmail.com>
2023-07-02 20:10:15 +0700

    add sound in game + animate obstacle, monster + change graphics

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -4,19 +4,25 @@ public class PlayerCollision : MonoBehaviour
 {
     public int moneyGet = 0;
 
+    [SerializeField] private AudioSource collectSound;
+    [SerializeField] private AudioSource dieSound;
+    
     private void OnTriggerEnter2D(Collider2D collision) 
     {
         switch (collision.GetComponent<TerrainType>().terrainType)
         {
             case TerrainType.TerrainTypes.obstacle:
+                dieSound.Play();
                 FinishGameManager.Instance.FinishGame();
                 break;
             
             case TerrainType.TerrainTypes.monster:
+                dieSound.Play();
                 FinishGameManager.Instance.FinishGame();
                 break;
 
             case TerrainType.TerrainTypes.money:
+                collectSound.Play();
                 moneyGet += 1;
                 MoneyManager.Instance.AddMoney(1);
                 Destroy(collision.gameObject);
